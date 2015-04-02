359bb36c1dd3e2658774097d184b5547b2a5d31c
Kelsey W <kwicento@ualberta.ca>
2015-04-01 23:48:13 -0600

    Changed the markers, number prints properly now

--- ClassDiscuss/app/src/main/java/com/example/cmput401/classdiscuss/CampusBuilding.java
+++ ClassDiscuss/app/src/main/java/com/example/cmput401/classdiscuss/CampusBuilding.java
@@ -1,10 +1,20 @@
 package com.example.cmput401.classdiscuss;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.LatLngBounds;
+
 
 /**
  * Created by Kelsey on 25/03/2015.
@@ -14,9 +24,11 @@ public class CampusBuilding {
     private int numPeople;
     private GoogleMap mMap;
     private Marker marker;
+    private Context context;
 
-    public CampusBuilding(LatLng southwest, LatLng northeast, LatLng center, GoogleMap map)
+    public CampusBuilding(LatLng southwest, LatLng northeast, LatLng center, GoogleMap map, Context c)
     {
+        context = c;
         mMap = map;
         marker = map.addMarker(
                 new MarkerOptions().position(center)
@@ -42,8 +54,25 @@ public class CampusBuilding {
         else
         {
             marker.setVisible(true);
-            marker.setTitle(String.valueOf(numPeople));
-            marker.showInfoWindow();
+            drawMarkerNumber(String.valueOf(numPeople));
         }
+    }
+
+    public void drawMarkerNumber(String numStr)
+    {
+        Typeface typeface = Typeface.create("Helvetica",Typeface.NORMAL);
+        Bitmap markerImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_marker).copy(Bitmap.Config.ARGB_8888, true);
+        Canvas canvas = new Canvas(markerImage);
+        Paint paint = new Paint();
+        paint.setTextSize(22);
+        paint.setTypeface(typeface);
+        paint.setAntiAlias(true);
+        paint.setTextAlign(Paint.Align.CENTER);
+        int x = canvas.getWidth()/2;
+        int y = (markerImage.getHeight()/2) + 5;
+        canvas.drawText(numStr, x, y, paint); // paint defines the text color, stroke width, size
+        BitmapDrawable draw = new BitmapDrawable(context.getResources(), markerImage);
+        Bitmap drawBmp = draw.getBitmap();
+        marker.setIcon(BitmapDescriptorFactory.fromBitmap(drawBmp));
     }
 }
