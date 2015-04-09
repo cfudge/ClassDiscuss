8f52431a9a89cba56bee6a853a42f0e0b8956c40
Cole Fudge <cfudge@ualberta.ca>
2015-04-09 10:31:28 -0600

    Created ChatActivityTest

---
+++ ClassDiscuss/app/src/androidTest/java/com/example/cmput401/classdiscuss/ChatActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.cmput401.classdiscuss;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by CMDF_Alien on 4/9/2015.
+ */
+public class ChatActivityTest extends ActivityInstrumentationTestCase2<ChatActivity> {
+
+    public ChatActivityTest(){
+        super(ChatActivity.class);
+    }
+
+    
+
+}
